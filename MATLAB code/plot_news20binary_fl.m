6dc5a2c1d4fe176d3df420fa80fd1cbb353b9f12
lifei16 <59161085+lifei16@users.noreply.github.com>
2020-01-09 17:35:30 +0800

    Update plot_news20binary_fl.m

--- MATLAB code/plot_news20binary_fl.m
+++ MATLAB code/plot_news20binary_fl.m
@@ -28,4 +28,4 @@ title('news20binary');
 %legend('ASGARD-fc','DLRCSGR-eps','DLRCSGR-m','SMART-CD');
 legend('ASGARD-DL','IPALM-APPROX','SMART-CD','IPALM-KATYUSHA');
 set(gcf,'Position',[10 10 400 400]);
-saveas(gcf,[pwd '/my plots/fl_news20binary.eps']);
+saveas(gcf,[pwd '/my plots/fl_news20binary.eps'],'epsc');
