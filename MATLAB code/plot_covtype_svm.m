71b0faec702790188ee0897ca7cf3b8faa2c1a90
lifei16 <59161085+lifei16@users.noreply.github.com>
2020-02-01 17:21:51 +0800

    Update plot_covtype_svm.m

--- MATLAB code/plot_covtype_svm.m
+++ MATLAB code/plot_covtype_svm.m
@@ -15,7 +15,7 @@ plot(DLRCSGR_eps_svm_covtypem(1:s2,2),log10((abs(DLRCSGR_eps_svm_covtypem(1:s2,3
 plot(SMART_CD_svm_covtypem(1:s3,2),log10((abs(SMART_CD_svm_covtypem(1:s3,5)- f_opt)/f_opt)),'--x','LineWidth',2);
 %plot(DLRCSGR_m_svm_covtypem(1:2:52,2),log10((abs(DLRCSGR_m_svm_covtypem(1:2:52,3)- f_opt)/f_opt)),'--o','LineWidth',2);
 %hold on
-plot(KATYUSHA_svm_covtypem(1:s4,2),log10((abs(KATYUSHA_svm_covtypem(1:s4,5)- f_opt)/f_opt)),'--o','LineWidth',2);
+plot(KATYUSHA_svm_covtypem(1:s4,2),log10((abs(KATYUSHA_svm_covtypem(1:s4,3)- f_opt)/f_opt)),'--o','LineWidth',2);
 %plot(ADMM_svm_covtypem(:,2),ADMM_svm_covtypem(:,6),'LineWidth',5);
 %hold on
 %plot(CVX_svm_covtypem(:,3),log10((abs(CVX_svm_covtypem(:,1)- f_opt)/f_opt)),'--+','LineWidth',2);
