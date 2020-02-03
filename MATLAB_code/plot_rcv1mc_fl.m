2806b0021b32e6034785aa9898db18da0ac8b93c
lifei16 <59161085+lifei16@users.noreply.github.com>
2020-02-03 10:53:36 +0800

    Update plot_rcv1mc_fl.m

--- MATLAB_code/plot_rcv1mc_fl.m
+++ MATLAB_code/plot_rcv1mc_fl.m
@@ -1,25 +1,25 @@
 f_opt= 3.24421077;
-ASGARD_fc_fl_rcv1mcmc= importdata('../IPALM/results/DLRCSGR2_FL_outer_rcv1mcmctau_47236',' ');
-DLRCSGR_eps_fl_rcv1mcmc= importdata('../IPALM/results/DLRCSGR3_FL_outer_rcv1mcmctau_1',' ');
-SMART_CD_fl_rcv1mcmc= importdata('../IPALM/results/PDCD_FL_outer_rcv1mcmctau_1',' ');
-KATYUSHA_fl_rcv1mcmc= importdata('../IPALM/results/ALM_FL_outer_rcv1mcmctau_250',' ');
+ASGARD_fc_fl_rcv1mc= importdata('../IPALM/results/DLRCSGR2_FL_outer_rcv1mctau_47236',' ');
+DLRCSGR_eps_fl_rcv1mc= importdata('../IPALM/results/DLRCSGR3_FL_outer_rcv1mctau_1',' ');
+SMART_CD_fl_rcv1mc= importdata('../IPALM/results/PDCD_FL_outer_rcv1mctau_1',' ');
+KATYUSHA_fl_rcv1mc= importdata('../IPALM/results/ALM_FL_outer_rcv1mctau_250',' ');
 s1= size(ASGARD_fc_fl_rcv1mc,1);
 s2= size(DLRCSGR_eps_fl_rcv1mc,1);
 s3= size(SMART_CD_fl_rcv1mc,1);
 s4= size(KATYUSHA_fl_rcv1mc,1);
-plot(ASGARD_fc_fl_rcv1mcmc(1:3:s1,2),log10((abs(ASGARD_fc_fl_rcv1mcmc(1:3:s1,3)- f_opt)/f_opt)),'-->','LineWidth',2);
+plot(ASGARD_fc_fl_rcv1mc(1:3:s1,2),log10((abs(ASGARD_fc_fl_rcv1mc(1:3:s1,3)- f_opt)/f_opt)),'-->','LineWidth',2);
 hold on
-plot(DLRCSGR_eps_fl_rcv1mcmc(1:15:s2,2),log10((abs(DLRCSGR_eps_fl_rcv1mcmc(1:15:s2,3)- f_opt)/f_opt)),'--s','LineWidth',2);
+plot(DLRCSGR_eps_fl_rcv1mc(1:15:s2,2),log10((abs(DLRCSGR_eps_fl_rcv1mc(1:15:s2,3)- f_opt)/f_opt)),'--s','LineWidth',2);
 hold on
-%plot(DLRCSGR_m_fl_rcv1mcmc(:,2),log10((abs(DLRCSGR_m_fl_rcv1mcmc(:,3)- f_opt)/f_opt)),'--o','LineWidth',2);
+%plot(DLRCSGR_m_fl_rcv1mc(:,2),log10((abs(DLRCSGR_m_fl_rcv1mcmc(:,3)- f_opt)/f_opt)),'--o','LineWidth',2);
 %hold on
-plot(SMART_CD_fl_rcv1mcmc(1:20:s3,2),log10((abs(SMART_CD_fl_rcv1mcmc(1:20:s3,3)- f_opt)/f_opt)),'--x','LineWidth',2);
+plot(SMART_CD_fl_rcv1mc(1:20:s3,2),log10((abs(SMART_CD_fl_rcv1mc(1:20:s3,3)- f_opt)/f_opt)),'--x','LineWidth',2);
 hold on
-plot(KATYUSHA_fl_rcv1mcmc(1:6:s4,2),log10((abs(KATYUSHA_fl_rcv1mcmc(1:6:s4,3)- f_opt)/f_opt)),'--o','LineWidth',2);
+plot(KATYUSHA_fl_rcv1mc(1:6:s4,2),log10((abs(KATYUSHA_fl_rcv1mc(1:6:s4,3)- f_opt)/f_opt)),'--o','LineWidth',2);
 %hold on
-%plot(ADMM_fl_rcv1mcmc(:,2),ADMM_fl_rcv1mcmc(:,6),'LineWidth',5);
+%plot(ADMM_fl_rcv1mc(:,2),ADMM_fl_rcv1mc(:,6),'LineWidth',5);
 %hold on
-%plot(CVX_fl_rcv1mcmc(:,3),log10(abs(CVX_fl_rcv1mcmc(:,1)- f_opt)),'--+','LineWidth',5);
+%plot(CVX_fl_rcv1mc(:,3),log10(abs(CVX_fl_rcv1mc(:,1)- f_opt)),'--+','LineWidth',5);
 ylim([-4.5 4]);
 xlim([0 1500]);
 xlabel('time');
@@ -28,4 +28,4 @@ title('rcv1mcmc');
 %legend('ASGARD-fc','DLRCSGR-eps','DLRCSGR-m','SMART-CD');
 legend('ASGARD-DL','IPALM-APPROX','SMART-CD','IPALM-KATYUSHA');
 set(gcf,'Position',[10 10 400 400]);
-saveas(gcf,'myplots/fl_rcv1mcmc.eps','epsc');
+saveas(gcf,'myplots/fl_rcv1mc.eps','epsc');
