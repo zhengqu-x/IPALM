e89ceaf112d8f99dd8da3797e0e6f6748c579709
lifei16 <59161085+lifei16@users.noreply.github.com>
2021-01-04 17:07:59 +0800

    Update plot_w8a_svm.m

--- MATLAB_code/plot_w8a_svm.m
+++ MATLAB_code/plot_w8a_svm.m
@@ -9,16 +9,16 @@ s2= size(DLRCSGR_eps_svm_w8am,1);
 s3= size(SMART_CD_svm_w8am,1);
 s4= size(KATYUSHA_svm_w8am,1);
 s5= size(ADMM_svm_w8am,1);
-plot(ASGARD_fc_svm_w8am(1:5:s1,2),log10((abs(ASGARD_fc_svm_w8am(1:5:s1,3)- f_opt)/f_opt)),'-->','LineWidth',2);
+plot(ASGARD_fc_svm_w8am(1:10:s1,2),log10((abs(ASGARD_fc_svm_w8am(1:10:s1,3)- f_opt)/f_opt)),'-->','LineWidth',2);
 hold on
-plot(DLRCSGR_eps_svm_w8am(1:3:s2,2),log10((abs(DLRCSGR_eps_svm_w8am(1:3:s2,3)- f_opt)/f_opt)),'--s','LineWidth',2);
+plot(DLRCSGR_eps_svm_w8am(1:10:s2,2),log10((abs(DLRCSGR_eps_svm_w8am(1:10:s2,3)- f_opt)/f_opt)),'--s','LineWidth',2);
 hold on
-plot(ADMM_svm_w8am(1:10:s5,2),log10((abs(ADMM_svm_w8am(1:10:s5,3)-f_opt)/f_opt)),'--+','LineWidth',2);
+plot(ADMM_svm_w8am(1:30:s5,2),log10((abs(ADMM_svm_w8am(1:30:s5,3)-f_opt)/f_opt)),'--+','LineWidth',2);
 hold on
 plot(SMART_CD_svm_w8am(1:10:s3,2),log10((abs(SMART_CD_svm_w8am(1:10:s3,5)- f_opt)/f_opt)),'--x','LineWidth',2);
 %plot(DLRCSGR_m_svm_w8am(1:2:s4,2),log10((abs(DLRCSGR_m_svm_w8am(1:2:s4,3)- f_opt)/f_opt)),'--o','LineWidth',2);
 %hold on
-plot(KATYUSHA_svm_w8am(1:3:s4,2),log10((abs(KATYUSHA_svm_w8am(1:3:s4,3)- f_opt)/f_opt)),'--o','LineWidth',2);
+plot(KATYUSHA_svm_w8am(1:10:s4,2),log10((abs(KATYUSHA_svm_w8am(1:10:s4,3)- f_opt)/f_opt)),'--o','LineWidth',2);
 %plot(ADMM_svm_w8am(:,2),ADMM_svm_w8am(:,6),'LineWidth',5);
 %hold on
 %plot(CVX_svm_w8am(:,3),log10((abs(CVX_svm_w8am(:,1)- f_opt)/f_opt)),'--+','LineWidth',2);
