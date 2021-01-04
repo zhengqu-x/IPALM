6ced1823e20b12b132c64964460a5aeed8c4d27f
lifei16 <59161085+lifei16@users.noreply.github.com>
2021-01-04 17:37:58 +0800

    Update plot_rcv1_lasso.m

--- MATLAB_code/plot_rcv1_lasso.m
+++ MATLAB_code/plot_rcv1_lasso.m
@@ -2,12 +2,16 @@ f_opt= 2.10978;
 ASGARD_fc_l1l_rcv1= importdata('../IPALM/results/DLRCSGR2_LAD_outer_rcv1tau_47236',' ');
 DLRCSGR_eps_l1l_rcv1= importdata('../IPALM/results/DLRCSGR3_LAD_outer_rcv1tau_1',' ');
 SMART_CD_l1l_rcv1= importdata('../IPALM/results/PDCD_LAD_outer_rcv1tau_1',' ');
+ADMM_l1l_rcv1= importdata('../IPALM/results/ADMM_LAD_rcv1_beta_1',' ');
 s1= size(ASGARD_fc_l1l_rcv1,1);
 s2= size(DLRCSGR_eps_l1l_rcv1,1);
 s3= size(SMART_CD_l1l_rcv1,1);
+s4= size(ADMM_l1l_rcv1,1);
 plot(ASGARD_fc_l1l_rcv1(1:2:s1,2),log10((abs(ASGARD_fc_l1l_rcv1(1:2:s1,3)- f_opt)/f_opt)),'-->','LineWidth',2);
 hold on
 plot(DLRCSGR_eps_l1l_rcv1(1:10:s2,2),log10((abs(DLRCSGR_eps_l1l_rcv1(1:10:s2,3)- f_opt)/f_opt)),'--s','LineWidth',2);
+hold on
+plot(ADMM_l1l_rcv1(1:20:s4,2),log10(abs(ADMM_l1l_rcv1(1:20:s4,3)- f_opt)/f_opt),'--+','LineWidth',2);
 hold on
 plot(SMART_CD_l1l_rcv1(1:20:s3,2),log10((abs(SMART_CD_l1l_rcv1(1:20:s3,3)- f_opt)/f_opt)),'--x','LineWidth',2);
 %plot(DLRCSGR_m_l1l_rcv1(1:2:52,2),log10((abs(DLRCSGR_m_l1l_rcv1(1:2:52,3)- f_opt)/f_opt)),'--o','LineWidth',2);
@@ -20,6 +24,6 @@ xlim([0 1000]);
 xlabel('time');
 ylabel('log|F(x)- F^*|/F^*');
 title('rcv1');
-legend({'ASGARD-DL','IPALM-APPROX','SMART-CD'},'interpreter','latex','Fontsize',10);
+legend({'ASGARD-DL','IPALM-APPROX','LADMM','SMART-CD'},'interpreter','latex','Fontsize',10);
 set(gcf,'Position',[10 10 400 400]);
 saveas(gcf,'myplots/lasso_rcv1.eps','epsc');
