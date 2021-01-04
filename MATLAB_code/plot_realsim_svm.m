deca4f4b28adf6c072ba6ae6c1174dc1e429b22f
lifei16 <59161085+lifei16@users.noreply.github.com>
2021-01-04 17:44:23 +0800

    Update plot_realsim_svm.m

--- MATLAB_code/plot_realsim_svm.m
+++ MATLAB_code/plot_realsim_svm.m
@@ -3,17 +3,21 @@ ASGARD_fc_svm_realsim= importdata('../IPALM/results/DLRCSGR2_SVM_outer_realsimta
 DLRCSGR_eps_svm_realsim= importdata('../IPALM/results/DLRCSGR3_SVM_outer_realsimtau_1',' ');
 SMART_CD_svm_realsim= importdata('../IPALM/results/PDCD_SMSVM_SVM_outer_realsimtau_1',' ');
 KATYUSHA_svm_realsim= importdata('../IPALM/results/ALM_SVM_outer_realsimtau_268',' ');
+ADMM_svm_realsim= importdata('../IPALM/results/ADMM_SVM_realsim_beta_2',' ');
 s1= size(ASGARD_fc_svm_realsim,1);
 s2= size(DLRCSGR_eps_svm_realsim,1);
 s3= size(SMART_CD_svm_realsim,1);
 s4= size(KATYUSHA_svm_realsim,1);
+s5= size(ADMM_svm_realsim,1);
 plot(ASGARD_fc_svm_realsim(1:4:s1,2),log10((abs(ASGARD_fc_svm_realsim(1:4:s1,3)- f_opt)/f_opt)),'-->','LineWidth',2);
 hold on
 plot(DLRCSGR_eps_svm_realsim(1:9:s2,2),log10((abs(DLRCSGR_eps_svm_realsim(1:9:s2,3)- f_opt)/f_opt)),'--s','LineWidth',2);
 hold on
+plot(ADMM_svm_realsim(1:10:s5,2),log10(abs(ADMM_svm_realsim(1:10:s5,3)- f_opt)/f_opt),'--+','LineWidth',2);
+hold on
 plot(SMART_CD_svm_realsim(1:2:s3,2),log10((abs(SMART_CD_svm_realsim(1:2:s3,5)- f_opt)/f_opt)),'--x','LineWidth',2);
 %plot(DLRCSGR_m_svm_realsim(1:2:52,2),log10((abs(DLRCSGR_m_svm_realsim(1:2:52,3)- f_opt)/f_opt)),'--o','LineWidth',2);
-%hold on
+hold on
 plot(KATYUSHA_svm_realsim(1:7:s4,2),log10((abs(KATYUSHA_svm_realsim(1:7:s4,3)- f_opt)/f_opt)),'--o','LineWidth',2);
 %plot(ADMM_svm_realsim(:,2),ADMM_svm_realsim(:,6),'LineWidth',5);
 %hold on
@@ -23,7 +27,7 @@ xlim([0 3600]);
 xlabel('time');
 ylabel('log|F(x)- F^*|/F^*');
 title('realsim');
-legend('ASGARD-DL','IPALM-APPROX','SMART-CD','IPALM-KATYUSHA');
+legend('ASGARD-DL','IPALM-APPROX','LADMM','SMART-CD','IPALM-KATYUSHA');
 %legend('ASGARD-DL','IPALM-APPROX','SMART-CD');
 set(gcf,'Position',[10 10 400 400]);
 saveas(gcf,'myplots/svm_realsim.eps','epsc');
