4deae789de9049d0ee3db6998cab125d97e05853
lifei16 <59161085+lifei16@users.noreply.github.com>
2021-01-04 17:39:15 +0800

    Update plot_rcv1mc_bp.m

--- MATLAB_code/plot_rcv1mc_bp.m
+++ MATLAB_code/plot_rcv1mc_bp.m
@@ -2,9 +2,11 @@ f_opt= 1.64446161;
 ASGARD_fc_bp_rcv1mc= importdata('../IPALM/results/ALM2_BP_outer_rcv1mctau_47236',' ');
 DLRCSGR_eps_bp_rcv1mc= importdata('../IPALM/results/ALM3_BP_outer_rcv1mctau_1',' ');
 SMART_CD_bp_rcv1mc= importdata('../IPALM/results/PDCD_I_BP_outer_rcv1mctau_1',' ');
+ADMM_bp_rcv1mc= importdata('../IPALM/results/ADMM_BP_rcv1mc_beta_1',' ');
 s1= size(ASGARD_fc_bp_rcv1mc,1);
 s2= size(DLRCSGR_eps_bp_rcv1mc,1);
 s3= size(SMART_CD_bp_rcv1mc,1);
+s4= size(ADMM_bp_rcv1mc,1);
 figure(1);
 plot(ASGARD_fc_bp_rcv1mc(1:10:s1,2),log10(abs(ASGARD_fc_bp_rcv1mc(1:10:s1,5)- f_opt)/f_opt),'-->','LineWidth',2);
 hold on
@@ -12,6 +14,8 @@ plot(DLRCSGR_eps_bp_rcv1mc(1:10:s2,2),log10(abs(DLRCSGR_eps_bp_rcv1mc(1:10:s2,5)
 hold on
 %plot(DLRCSGR_m_bp_rcv1mc(:,2),log10(abs(DLRCSGR_m_bp_rcv1mc(:,5)- f_opt)/f_opt),'--o','LineWidth',2);
 %hold on
+plot(ADMM_bp_rcv1mc(1:20:s4,2),log10(abs(ADMM_bp_rcv1mc(1:20:s4,3)- f_opt)/f_opt),'--+','LineWidth',2);
+hold on
 plot(SMART_CD_bp_rcv1mc(1:120:s3,2),log10((abs(SMART_CD_bp_rcv1mc(1:120:s3,4)- f_opt)/f_opt)),'--x','LineWidth',2);
 %hold on
 %plot(ADMM_bp_rcv1mc(:,2),ADMM_bp_rcv1mc(:,6),'LineWidth',5);
@@ -24,7 +28,7 @@ ylabel('log|F(x)- F^*|/F^*');
 title('rcv1mc');
 %legend('ASGARD-fc','ASGARD-nurc');
 %legend('ASGARD-fc','DLRCSGR-eps','DLRCSGR-m','SMART-CD','CVX');
-legend({'ASGARD-DL','IPALM-APPROX','SMART-CD'},'interpreter','latex','Fontsize',10);
+legend({'ASGARD-DL','IPALM-APPROX','LADMM','SMART-CD'},'interpreter','latex','Fontsize',10);
 set(gcf,'Position',[10 10 350 400]);
 saveas(figure(1),'myplots/bp_rcv1mc_obj.eps','epsc');
 figure(2);
@@ -34,6 +38,8 @@ plot(DLRCSGR_eps_bp_rcv1mc(1:10:s2,2),log10(DLRCSGR_eps_bp_rcv1mc(1:10:s2,4)),'-
 hold on
 %plot(DLRCSGR_m_bp_rcv1mc(:,2),log10(DLRCSGR_m_bp_rcv1mc(:,4)),'--o','LineWidth',2);
 %hold on
+plot(ADMM_bp_rcv1mc(1:20:s4,2),log10(ADMM_bp_rcv1mc(1:20:s4,4)),'--+','LineWidth',2);
+hold on
 plot(SMART_CD_bp_rcv1mc(1:120:s3,2),log10(SMART_CD_bp_rcv1mc(1:120:s3,3)),'--x','LineWidth',2);
 xlim([0 3000]);
 ylim([-7 4]);
@@ -41,7 +47,7 @@ xlabel('time');
 ylabel('log|Ax- b|');
 title('rcv1mc');
 %legend('ASGARD-fc','ASGARD-nurc');
-legend({'ASGARD-DL','IPALM-APPROX','SMART-CD'},'interpreter','latex','Fontsize',10);
+legend({'ASGARD-DL','IPALM-APPROX','LADMM','SMART-CD'},'interpreter','latex','Fontsize',10);
 %legend({'ASGARD-fc','DLRCSGR-eps','DLRCSGR-m','CVX'},'interpreter','latex','Fontsize',10);
 set(gcf,'Position',[10 10 350 400]);
 saveas(figure(2),'myplots/bp_rcv1mc_infeas.eps','epsc');
