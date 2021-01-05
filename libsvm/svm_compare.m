593a86a518bc1a30e3f3d9dbe0a5c133c0453bad
lifei16 <59161085+lifei16@users.noreply.github.com>
2021-01-05 16:56:13 +0800

    Update svm_compare.m

--- libsvm/svm_compare.m
+++ libsvm/svm_compare.m
@@ -1,4 +1,4 @@
-[b, A] = libsvmread('w8a');
+[b, A] = libsvmread('w8a.txt');
 [b2, A2]= libsvmread('w8a.t');
 tic
 model = svmtrain(b, A );
@@ -9,15 +9,15 @@ toc
 n1= size(A,2);
 n2= size(A2,2);
 %copy the output x from the folder "/IPALM/results"
-%a7a_x= importdata("L_Katyusha_x_SVM_outer_a7atau_126");
-%a8a_x= importdata("L_Katyusha_x_SVM_outer_a8atau_150");
-%a9a_x= importdata("L_Katyusha_x_SVM_outer_a9atau_180");
-%ijcnn1_x= importdata("L_Katyusha_x_SVM_outer_ijcnn1tau_223");
-%w6a_x= importdata("L_Katyusha_x_SVM_outer_w6atau_131");
-%w7a_x= importdata("L_Katyusha_x_SVM_outer_w7atau_157");
-w8a_x= importdata("L_Katyusha_x_SVM_outer_w8atau_223");
+%x= importdata("L_Katyusha_x_SVM_outer_a7atau_126");
+%x= importdata("L_Katyusha_x_SVM_outer_a8atau_150");
+%x= importdata("L_Katyusha_x_SVM_outer_a9atau_180");
+%x= importdata("L_Katyusha_x_SVM_outer_ijcnn1tau_223");
+%x= importdata("L_Katyusha_x_SVM_outer_w6atau_131");
+%x= importdata("L_Katyusha_x_SVM_outer_w7atau_157");
+x= importdata("L_Katyusha_x_SVM_outer_w8atau_223");
 n= min(n1,n2);
-b_test= A2(:,1:n)*w8a_x(1:n,1)+ w8a_x(n1+ 1,1);
+b_test= A2(:,1:n)*x(1:n,1)+ x(n1+ 1,1);
 com= b2.*b_test;
 accuracy= size(com(com>=0),1)/size(A2,1);
 %accuracy is the accuracy of IPALM
