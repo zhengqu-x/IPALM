60e4836c07124219c16d034f283a89f8da3cd75c
lifei16 <59161085+lifei16@users.noreply.github.com>
2021-01-06 15:01:50 +0800

    Update qcqp.m

--- CVX/qcqp.m
+++ CVX/qcqp.m
@@ -1,4 +1,11 @@
-A = importdata('qcqp1_A.mat'); b = importdata('qcqp1_b.mat');
+A = importdata('qcqp1_A.mat'); 
+%A1 = importdata('qcqp4_A1.mat'); 
+%A2 = importdata('qcqp4_A2.mat'); 
+%A3 = importdata('qcqp4_A3.mat'); 
+%A4 = importdata('qcqp4_A4.mat'); 
+%A5 = importdata('qcqp4_A5.mat'); 
+%A= [A1;A2;A3;A4;A5];
+b = importdata('qcqp1_b.mat');
 m = size(A,1)/size(A,2)-1 ;
 n = size(A,2); 
 e= ones(n,1);
